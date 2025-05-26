b4b3784a0ee50481cdc369a583cc5af5cdd8fc35
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-05-26 22:49:02 +0300

    fixing lint

--- tests/gendiff_test.py
+++ tests/gendiff_test.py
@@ -2,6 +2,9 @@ import os
 
 from gendiff.gendiff_description import generate_diff
 
+ff1_yaml = 'test_data/ff1_yaml.yml'
+ff2_yaml = 'test_data/ff2_yaml.yml'
+
 
 def test_generate_diff_json():
     file1_path = os.path.join(os.path.dirname(__file__), 'test_data/ff1.json')
@@ -19,8 +22,8 @@ def test_generate_diff_json():
 
 
 def test_generate_diff_yml():
-    file1_path = os.path.join(os.path.dirname(__file__), 'test_data/ff1_yaml.yml')
-    file2_path = os.path.join(os.path.dirname(__file__), 'test_data/ff2_yaml.yml')
+    file1_path = os.path.join(os.path.dirname(__file__), ff1_yaml)
+    file2_path = os.path.join(os.path.dirname(__file__), ff2_yaml)
     expected_diff = """{
   - follow: False
     host: hexlet.io
