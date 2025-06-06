82e84b07d60c1675431899628ba38d0990302796
Ilya_M <iliya.miroshnichenko@yandex.ru>
2025-06-06 15:33:01 +0300

    added templates to plain.py

--- gendiff/formatters/plain.py
+++ gendiff/formatters/plain.py
@@ -1,9 +1,6 @@
-ADDED = 'was added with value:'
-FROM = 'was updated. From'
-CV = '[complex value]'
-P = 'Property'
-T = ' to '
-
+REM_TEMPL = "Property '{}' was removed"
+UPD_TEMPL = "Property '{}' was updated. From {} to {}"
+ADD_TEMPL = "Property '{}' was added with value: {}"
 
 def format_plain(diff_tree, path=""):
     lines = []
@@ -12,18 +9,13 @@ def format_plain(diff_tree, path=""):
         cur_pth = f"{path}.{node.key}" if path else node.key
 
         if node.type == 'added':
-            if isinstance(node.value, dict):
-                lines.append(f"{P} '{cur_pth}' {ADDED} {CV}")
-            else:
-                lines.append(f"{P} '{cur_pth}' {ADDED} {frm_v(node.value)}")
+            lines.append(ADD_TEMPL.format(cur_pth, frm_v(node.value)))
         elif node.type == 'removed':
-            lines.append(f"{P} '{cur_pth}' was removed")
+            lines.append(REM_TEMPL.format(cur_pth))
         elif node.type == 'changed':
             old_val = frm_v(node.value[0])
             new_val = frm_v(node.value[1])
-            lines.append(
-                f"Property '{cur_pth}' was updated. From {old_val} to {new_val}"
-            )
+            lines.append(UPD_TEMPL.format(cur_pth, old_val, new_val))
         elif node.type == 'nested':
             lines.append(format_plain(node.children, cur_pth))
 
